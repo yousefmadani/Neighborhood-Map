2d2a082b50c36088705bf0c76c320684e801a426
Yousef <yousef@live.ca>
2016-12-27 15:00:57 +0300

    Added observable array

--- library/jscript.js
+++ library/jscript.js
@@ -1,44 +1,53 @@
-var map, i, marker, myinfowindow;
-var locations=
-[{title: 'Caffe Nero', location: {lat: 51.277588, lng: 1.082901}},
-{title: 'Starbucks', location: {lat: 51.278085, lng: 1.081973 }},
-{title: 'Cafe Turquoise', location: {lat: 51.278627, lng:1.081455}},
-{title: 'Burgate Coffee House', location: {lat:51.279024, lng:1.081649}},
-{title: 'Pret A Manger', location: {lat: 51.278771, lng:1.080600}},
-{title: 'Cafe St Pierre', location: {lat: 51.280906, lng:1.076769}},
-{title: 'Costa Coffee', location: {lat: 51.279324, lng:1.079699}},
-{title: 'Patisserie Valerie', location: {lat: 51.279915, lng:1.078733}}];
+    var map, i, marker, myinfowindow;
 
-var styles =[{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#fffaf4"},{"saturation":"0"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff8b00"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"},{"color":"#ff0000"}]},{"featureType":"landscape.natural.landcover","elementType":"all","stylers":[{"color":"#ff0000"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#a0b8db"},{"visibility":"on"}]}]; //Styles from https://snazzymaps.com/style/42415/bema-cafe but "poi.business" has been removed
+    var locations=
+    [{title: 'Caffe Nero', location: {lat: 51.277588, lng: 1.082901}},
+    {title: 'Starbucks', location: {lat: 51.278085, lng: 1.081973 }},
+    {title: 'Cafe Turquoise', location: {lat: 51.278627, lng:1.081455}},
+    {title: 'Burgate Coffee House', location: {lat:51.279024, lng:1.081649}},
+    {title: 'Pret A Manger', location: {lat: 51.278771, lng:1.080600}},
+    {title: 'Cafe St Pierre', location: {lat: 51.280906, lng:1.076769}},
+    {title: 'Costa Coffee', location: {lat: 51.279324, lng:1.079699}},
+    {title: 'Patisserie Valerie', location: {lat: 51.279915, lng:1.078733}}];
+
+    var styles =[{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#fffaf4"},{"saturation":"0"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff8b00"}]},{"featureType":"landscape.natural","elementType":"all","stylers":[{"visibility":"off"},{"color":"#ff0000"}]},{"featureType":"landscape.natural.landcover","elementType":"all","stylers":[{"color":"#ff0000"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#a0b8db"},{"visibility":"on"}]}]; //Styles from https://snazzymaps.com/style/42415/bema-cafe but "poi.business" has been removed
 
 
-function initMap(){
-    map = new google.maps.Map (document.getElementById('map'), {
-        center: {lat: 51.2785215, lng: 1.0790438},
-        zoom:16,
-        styles:styles
-    });
-
-    myinfowindow = new google.maps.InfoWindow();
-
-    for (i=0; i<locations.length; i++){
-        var cafeTitles = locations[i].title;
-        var cafePositions = locations[i].location;
-        //console.log(locations[i]); // check
-        marker = new google.maps.Marker({
-            map:map,
-            title: cafeTitles,
-            position: cafePositions,
-            //infowindow:myinfowindow
-            //icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    function initMap(){
+        map = new google.maps.Map (document.getElementById('map'), {
+            center: {lat: 51.2785215, lng: 1.0790438},
+            zoom:16,
+            styles:styles
         });
-    (function (marker, cafeTitles) {
-                google.maps.event.addListener(marker, "click", function (e) {
-                    myinfowindow.open(map, marker);
-                    //console.log(cafeTitles); //check
-                    myinfowindow.setContent(cafeTitles);
-                });
-            })(marker, cafeTitles);
 
-}
-}
+        myinfowindow = new google.maps.InfoWindow();
+
+        for (i=0; i<locations.length; i++){
+            var cafeTitles = locations[i].title;
+            var cafePositions = locations[i].location;
+            //console.log(locations[i]); // check
+    marker = new google.maps.Marker({
+        map:map,
+        title: cafeTitles,
+        position: cafePositions,
+        //icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    });
+    (function (marker, cafeTitles) {
+        google.maps.event.addListener(marker, "click", function () {
+            myinfowindow.open(map, marker);
+    //console.log(cafeTitles); //check
+    myinfowindow.setContent(cafeTitles);
+    });
+    })(marker, cafeTitles);
+    }
+
+    var ViewModel = function(){
+        var self = this;
+        self.myOA = ko.observableArray ();
+        for (i=0;i<locations.length;i++){
+            self.myOA.push(locations[i]);
+            //console.log(locations[i]);
+        }
+    };
+    ko.applyBindings(new ViewModel());
+    }
